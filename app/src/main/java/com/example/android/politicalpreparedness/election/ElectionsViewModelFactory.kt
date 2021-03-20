9c169fed5afd6b56817e144bc5c434bab88ac52b
bobtal <talevski.boban@gmail.com>
2021-03-20 21:02:45 +0100

    Completes ElectionsViewModelFactory implementation

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModelFactory.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModelFactory.kt
@@ -1,8 +1,19 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.database.ElectionDao
+import java.lang.IllegalArgumentException
 
-//TODO: Create Factory to generate ElectionViewModel with provided election datasource
-class ElectionsViewModelFactory: ViewModelProvider.Factory {
-
+//COMPLETE: Create Factory to generate ElectionViewModel with provided election datasource
+class ElectionsViewModelFactory(
+        private val database: ElectionDao
+): ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ElectionsViewModel::class.java)) {
+            return ElectionsViewModel(database) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
