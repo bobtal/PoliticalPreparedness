ddf3907b643d8420f60a8cd2ed0c39754c9efa77
bobtal <talevski.boban@gmail.com>
2021-03-20 23:34:24 +0100

    Implements recyclerviews in ElectionsFragment by ElectionListAdapter

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -4,26 +4,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.database.ElectionDatabase
+import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
+import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
+import com.example.android.politicalpreparedness.election.adapter.ElectionListener
+import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 
 class ElectionsFragment: Fragment() {
 
     //TODO: Declare ViewModel
+    private lateinit var viewModel: ElectionsViewModel
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
 
         //TODO: Add ViewModel values and create ViewModel
+        val application = requireNotNull(this.activity).application
+        val dataSource = ElectionDatabase.getInstance(application).electionDao
+        val viewModelFactory = ElectionsViewModelFactory(dataSource)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ElectionsViewModel::class.java)
 
         //TODO: Add binding values
+        val binding: FragmentElectionBinding =
+                DataBindingUtil.inflate(
+                        inflater, R.layout.fragment_election, container, false)
+        binding.viewModel = viewModel
 
         //TODO: Link elections to voter info
 
         //TODO: Initiate recycler adapters
+        val clickListener = {electionId : Int -> viewModel.onElectionClicked(electionId)}
+        val upcomingElectionsAdapter = ElectionListAdapter(ElectionListener(clickListener))
+        val savedElectionsAdapter = ElectionListAdapter(ElectionListener(clickListener))
 
         //TODO: Populate recycler adapters
+        viewModel.upcomingElections.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                upcomingElectionsAdapter.submitList(it)
+            }
+        })
 
+        viewModel.savedElections.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                savedElectionsAdapter.submitList(it)
+            }
+        })
+
+        return binding.root
     }
 
     //TODO: Refresh adapters when fragment loads
--- app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -1,23 +1,64 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.example.android.politicalpreparedness.databinding.ViewholderElectionBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ListItemElectionBinding
+//import com.example.android.politicalpreparedness.databinding.ViewholderElectionBinding
 import com.example.android.politicalpreparedness.network.models.Election
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
-class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
+class ElectionListAdapter(private val clickListener: ElectionListener) :
+        ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
         return ElectionViewHolder.from(parent)
     }
 
     //TODO: Bind ViewHolder
+    override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
+        val election = getItem(position) as Election
+        holder.bind(election, clickListener)
+    }
 
     //TODO: Add companion object to inflate ViewHolder (from)
+
 }
 
 //TODO: Create ElectionViewHolder
+class ElectionViewHolder(private var binding: ListItemElectionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Election, clickListener: ElectionListener) {
+        binding.election = item
+        binding.clickListener = clickListener
+        binding.executePendingBindings()
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): ElectionViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ListItemElectionBinding.inflate(layoutInflater, parent, false)
+            return ElectionViewHolder(binding)
+        }
+    }
+
+}
 
 //TODO: Create ElectionDiffCallback
+class ElectionDiffCallback : DiffUtil.ItemCallback<Election>() {
+    override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Election, newItem: Election): Boolean {
+        return oldItem == newItem
+    }
+}
 
 //TODO: Create ElectionListener
+class ElectionListener(val clickListener: (electionId: Int) -> Unit) {
+    fun onClick(election: Election) = clickListener(election.id)
+}
