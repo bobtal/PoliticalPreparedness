1664ec67651d8ad937739971a71169a983855405
bobtal <talevski.boban@gmail.com>
2021-03-21 11:31:10 +0100

    Updates ElectionListFragment related code to populate the recyclerviews

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -16,7 +16,7 @@ interface ElectionDao {
 
     //COMPLETE: Add select all election query
     @Query("SELECT * FROM election_table")
-    fun getAllElections() : List<Election>
+    suspend fun getAllElections() : List<Election>
 
     //COMPLETE: Add select single election query
     @Query("SELECT * FROM election_table WHERE id = :electionId")
